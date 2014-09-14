9136f15d940cfacf6145f854e64397e9af7fc074
t93 <dots64if@gmail.com>
2014-09-14 22:55:24 +0900

    Add user api

--- app/api/api.rb
+++ app/api/api.rb
@@ -3,21 +3,40 @@ class API < Grape::API
   format :json
 
   helpers do
+    def current_user
+      begin
+        User.find(params[:user_id])
+      rescue ActiveRecord::RecordNotFound
+        {}
+      end
+    end
   end
 
   resource :test do
     get '/hello' do
-      { hello: "world" }
+      { hello: 'world' }
     end
 
     get :hi do
-      "hi"
+      'hi'
     end
   end
 
   resource :user do
-    get '1' do
-      User.first
+    desc 'returns all users'
+    get do
+      User.all
+    end
+
+    params do
+      requires :user_id, type: Integer
+    end
+
+    desc 'returns user'
+    route_param :user_id do
+      get do
+        current_user
+      end
     end
   end
 end
