9e47d236fce64d538b8ac08bb6464e9a08942534
t93 <dots64if@gmail.com>
2014-10-01 21:29:17 +0900

    Fix batch

--- lib/tasks/migrate_pile_id.rb
+++ lib/tasks/migrate_pile_id.rb
@@ -1,12 +1,17 @@
 class Tasks::MigratePileId
   def self.execute
     Pile.all.each do |pile|
-      next if ContentsPile.find_by(content_id: pile.content_id)
-      contents_pile = ContentsPile.new({
-        pile_id: pile.id,
-        content_id: pile.content_id
-      })
-      contents_pile.save
+      if ActiveRecord::Base.connection.column_exists?(:piles, :content_id) && !ContentsPile.find_by(content_id: pile.content_id)
+          contents_pile = ContentsPile.new({
+            pile_id: pile.id,
+            content_id: pile.content_id
+          })
+        contents_pile.save
+      end
+
+      unless ContentsPile.find_by(pile_id: pile.id)
+        pile.destroy
+      end
     end
   end
 end
